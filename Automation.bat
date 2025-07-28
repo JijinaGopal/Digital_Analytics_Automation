8a5fcd34b001ed4c9516ee87df9cb1e64827c183
Jijina Gopal <jijinagopal15@gmail.com>
2025-07-28 16:55:17 +0530

    Auto update data - Mon 07/28/2025 16:55:16.97

--- Automation.bat
+++ Automation.bat
@@ -1,15 +1,7 @@
 @echo off
 
 
-cd "C:\Users\Administrator\Desktop\Automation\Digital_Analytics_WebApp"
-
-
-copy /Y "C:\Users\Administrator\Desktop\Automation\data\orders.csv" "data\orders.csv"
-copy /Y "C:\Users\Administrator\Desktop\Automation\data\order_items.csv" "data\order_items.csv"
-copy /Y "C:\Users\Administrator\Desktop\Automation\data\order_item_refunds.csv" "data\order_item_refunds.csv"
-copy /Y "C:\Users\Administrator\Desktop\Automation\data\products.csv" "data\products.csv"
-copy /Y "C:\Users\Administrator\Desktop\Automation\data\w_sessions.csv" "data\w_sessions.csv"
-copy /Y "C:\Users\Administrator\Desktop\Automation\data\website_pageviews.csv" "data\website_pageviews.csv"
+cd "C:\Users\Administrator\Desktop\Automation"
 
 
 git add .
