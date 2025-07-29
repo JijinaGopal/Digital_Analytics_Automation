b90d488b773379d59360129786cedef5dca09a74
Jijina Gopal <jijinagopal15@gmail.com>
2025-07-29 13:31:29 +0530

    Auto update after SQL cleaning - Tue 07/29/2025 13:31:29.39

--- Automation.bat
+++ Automation.bat
@@ -1,15 +1,18 @@
 @echo off
 
+echo STEP 1: Import website_sessions.csv into SQL Server and clean ...
+sqlcmd -S LAPTOP-2K6MH8QU\SQLEXPRESS -d E_Commerce_Project -E -i "C:\Users\Administrator\Desktop\Automation\full_import_and_clean.sql"
 
+
+echo STEP 2: Export cleaned w_sessions table to CSV...
+bcp "SELECT * FROM E_Commerce_Project.dbo.w_sessions" queryout "C:\Users\Administrator\Desktop\Automation\data\w_sessions.csv" -c -t, -S LAPTOP-2K6MH8QU\SQLEXPRESS -T -d E_Commerce_Project
+
+echo STEP 3: Git push changes to GitHub...
 cd "C:\Users\Administrator\Desktop\Automation"
-
-
-sqlcmd -S LAPTOP-2K6MH8QU\SQLEXPRESS -d E_Commerce_Project -E -i "C:\Users\Administrator\Desktop\Automation\SQL.sql"
-
-
 git add .
-git commit -m "Auto update data - %DATE% %TIME%"
+git commit -m "Auto update after SQL cleaning - %DATE% %TIME%"
 git push origin main
 
-echo Done! Streamlit Cloud will pick up changes.
+echo DONE. Streamlit Cloud will reflect the changes shortly.
 pause
+
--- SQL.sql
+++ SQL.sql
@@ -3,6 +3,28 @@ USE E_Commerce_Project
 
 
 --------------------------------------------DATA CLEANING--------------------------------------------------------
+-- Truncate and import website_sessions
+TRUNCATE TABLE website_sessions;
+BULK INSERT website_sessions
+FROM 'C:\Users\Administrator\Desktop\Automation\data\website_sessions.csv'
+WITH (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',
+    ROWTERMINATOR = '\n',
+    TABLOCK,
+    CODEPAGE = 'ACP'
+);
+
+
+
+
+-- Drop the table if it already exists 
+IF OBJECT_ID('dbo.w_sessions', 'U') IS NOT NULL
+    DROP TABLE dbo.w_sessions;
+
+
+select * from website_sessions into w_sessions
+
 ---updating nulls with unknown
 UPDATE w_sessions
 SET                           
@@ -18,3 +40,4 @@ WHERE
     utm_content IS NULL OR
     device_type IS NULL OR
     http_referer IS NULL;
+
